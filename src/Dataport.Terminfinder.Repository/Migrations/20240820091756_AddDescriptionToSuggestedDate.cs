ac6d6017963028d654dec4e0df88c7b5294691c9
Grünberg, Bent <bent.gruenberg@dataport.de>
2024-08-20 14:50:20 +0200

    feat: add description for `SuggestedDate`
    - add example command for migrations
    - fix typo

---
+++ src/Dataport.Terminfinder.Repository/Migrations/20240820091756_AddDescriptionToSuggestedDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dataport.Terminfinder.Repository.Migrations
+{
+    public partial class AddDescriptionToSuggestedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                schema: "public",
+                table: "suggesteddate",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                schema: "public",
+                table: "suggesteddate");
+        }
+    }
+}
---
+++ src/Dataport.Terminfinder.Repository/README.md
@@ -0,0 +1,9 @@
+﻿# Repository
+
+## Add Migration
+
+Navigate to the directory `Dataport.Terminfinder.Repository`
+
+```bash
+dotnet-ef migrations add <MIGRATIONNAME> --startup-project ../Dataport.Terminfinder.WebAPI
+```
