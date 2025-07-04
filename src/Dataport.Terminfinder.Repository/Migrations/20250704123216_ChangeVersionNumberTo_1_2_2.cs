41c2b58a6b87b9134b9b6b9e7b0787da2fa56cb5
Bent Grünberg <bent.gruenberg@dataport.de>
2025-07-04 14:41:14 +0200

    refactor: update version number

---
+++ src/Dataport.Terminfinder.Repository/Migrations/20250704123216_ChangeVersionNumberTo_1_2_2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
+
+#nullable disable
+
+namespace Dataport.Terminfinder.Repository.Migrations
+{
+    /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
+    public partial class ChangeVersionNumberTo_1_2_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlInhalt = "insert into public.appconfig(configkey, configvalue) select 'version','' WHERE NOT EXISTS(select 1 from public.appconfig WHERE configkey='version');";
+            migrationBuilder.Sql(sqlInhalt, suppressTransaction: true);
+
+            sqlInhalt = "insert into public.appconfig(configkey, configvalue) select 'builddate','' WHERE NOT EXISTS(select 1 from public.appconfig WHERE configkey='builddate');";
+            migrationBuilder.Sql(sqlInhalt, suppressTransaction: true);
+
+            sqlInhalt = "update public.appconfig set configvalue='1.2.2' WHERE configkey='version';";
+            migrationBuilder.Sql(sqlInhalt, suppressTransaction: true);
+
+            sqlInhalt = "update public.appconfig set configvalue='2025-07-04' WHERE configkey='builddate';";
+            migrationBuilder.Sql(sqlInhalt, suppressTransaction: true);
+        }
+    }
+}
