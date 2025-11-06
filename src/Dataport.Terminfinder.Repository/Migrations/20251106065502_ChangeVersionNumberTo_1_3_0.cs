7f1294f69334dfb609c01f7e7ce0fd9d9aad0992
gruenbbe <bent.gruenberg@dataport.de>
2025-11-06 08:43:22 +0100

    chore: update version number

---
+++ src/Dataport.Terminfinder.Repository/Migrations/20251106065502_ChangeVersionNumberTo_1_3_0.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
+
+#nullable disable
+
+namespace Dataport.Terminfinder.Repository.Migrations
+{
+    /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
+    public partial class ChangeVersionNumberTo_1_2_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlInhalt = "update public.appconfig set configvalue='1.3.0' WHERE configkey='version';";
+            migrationBuilder.Sql(sqlInhalt, suppressTransaction: true);
+
+            sqlInhalt = "update public.appconfig set configvalue='2025-11-06' WHERE configkey='builddate';";
+            migrationBuilder.Sql(sqlInhalt, suppressTransaction: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sqlInhalt = "update public.appconfig set configvalue='1.2.2' WHERE configkey='version';";
+            migrationBuilder.Sql(sqlInhalt, suppressTransaction: true);
+
+            sqlInhalt = "update public.appconfig set configvalue='2025-07-04' WHERE configkey='builddate';";
+            migrationBuilder.Sql(sqlInhalt, suppressTransaction: true);
+        }
+    }
+}
