eab0737093e85b19e5a441b9a67b5f55594d87ac
gruenbbe <bent.gruenberg@dataport.de>
2026-03-04 14:51:43 +0100

    refactor: apply sonarqube suggestions

--- src/Dataport.Terminfinder.Common/Jobs/IJob.cs
+++ src/Dataport.Terminfinder.Common/Jobs/IJob.cs
@@ -1,6 +1,13 @@
 ﻿namespace Dataport.Terminfinder.Common.Jobs;
 
+/// <summary>
+/// Job Interface
+/// </summary>
 public interface IJob
 {
+    /// <summary>
+    /// Execute Job
+    /// </summary>
+    /// <returns></returns>
     Task ExecuteAsync();
 }
