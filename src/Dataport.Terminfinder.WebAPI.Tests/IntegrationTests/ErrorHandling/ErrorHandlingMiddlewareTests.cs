7be9baf1f189a8018b8951ae3dac0eb9d48fae4c
gruenbbe <bent.gruenberg@dataport.de>
2026-07-20 14:17:24 +0200

    test: use `HostBuilder` instead of `WebHostBuilder`
    https://aka.ms/aspnet/deprecate/004

--- src/Dataport.Terminfinder.WebAPI.Tests/IntegrationTests/ErrorHandling/ErrorHandlingMiddlewareTests.cs
+++ src/Dataport.Terminfinder.WebAPI.Tests/IntegrationTests/ErrorHandling/ErrorHandlingMiddlewareTests.cs
@@ -7,31 +7,45 @@ namespace Dataport.Terminfinder.WebAPI.Tests.IntegrationTests.ErrorHandling;
 public class ErrorHandlingMiddlewareTests : BaseIntegrationTests
 {
     private TestServer _server;
+    private IHost _host;
     private HttpClient _client;
 
     [TestInitialize]
-    public void Setup()
+    public async Task Setup()
     {
         var mockLocalizer = new Mock<IStringLocalizer<ErrorMessageResources>>();
 
         var config = GetConfigurationBuilder();
-        var builder = new WebHostBuilder()
-            .UseStartup<Startup>()
-            .UseConfiguration(config)
-            .ConfigureServices(services =>
-            {
-                services.AddSingleton(mockLocalizer.Object);
-            })
-            .Configure(app =>
-            {
-                app.UseMiddleware<ErrorHandlingMiddleware>();
-                app.Map("/conflict", cfg =>
+        _host = new HostBuilder()
+            .ConfigureWebHost(builder =>
                 {
-                    cfg.Run(_ => throw new ConflictException(ErrorType.GeneralError));
-                });
-            });
+                    builder
+                        .UseTestServer()
+                        .UseStartup<Startup>()
+                        .UseConfiguration(config)
+                        .ConfigureServices(services =>
+                            {
+                                services.AddSingleton(mockLocalizer.Object);
+                            }
+                        )
+                        .Configure(app =>
+                            {
+                                app.UseMiddleware<ErrorHandlingMiddleware>();
+                                app.Map(
+                                    "/conflict",
+                                    cfg =>
+                                    {
+                                        cfg.Run(_ => throw new ConflictException(ErrorType.GeneralError));
+                                    }
+                                );
+                            }
+                        );
+                }
+            )
+            .Build();
 
-        _server = new TestServer(builder);
+        await _host.StartAsync();
+        _server = _host.GetTestServer();
         _client = _server.CreateClient();
     }
 
@@ -44,9 +58,15 @@ public class ErrorHandlingMiddlewareTests : BaseIntegrationTests
     }
 
     [TestCleanup]
-    public void Cleanup()
+    public async Task Cleanup()
     {
+        if (_server != null)
+        {
+            await _host.StopAsync();
+            _server.Dispose();
+        }
+
+        _host?.Dispose();
         _client.Dispose();
-        _server.Dispose();
     }
 }
